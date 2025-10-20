45ee5db5f4f7b46d68b0c1544cc797d2e3a70f08
002537387 <huang.in@northeastern.edu>
2025-10-20 16:35:12 -0400

    modify arrangement

--- DigitalUniversitySystem/src/Business/Profiles/Profile.java
+++ DigitalUniversitySystem/src/Business/Profiles/Profile.java
@@ -11,25 +11,25 @@ import Business.Person.Person;
  *
  * @author Ing-Ruei
  */
-public abstract class Profile {
-    Person person;
+    public abstract class Profile {
+        Person person;
     
-     public Profile(Person p){
-        person = p;       
-    }
-    
-     public abstract String getRole();
-    
-    public Person getPerson(){
-        return person;
-    }
-     
+        public Profile(Person p){
+            person = p;       
+        }
+
+        public abstract String getRole();
+
+            public Person getPerson(){
+            return person;
+        }
+
 
         public boolean isMatch(String id) {
-        if (person.getPersonId().equals(id)) {
-            return true;
+            if (person.getPersonId().equals(id)) {
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
 }
